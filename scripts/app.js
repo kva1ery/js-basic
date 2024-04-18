58a2fce8375bc66d3433dc5618d43cd953af25b9
Valeriy Kokorev <kokorev.valery@gmail.com>
2024-04-18 22:08:32 +0700

    Добавил рендер меню

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,12 @@
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
 
+/* page */
+const page = {
+    menu: document.querySelector('.menu__list'),
+}
+
+
 /* utils */
 
 function loadData() {
@@ -18,7 +24,47 @@ function saveData() {
 }
 
 
+/* render */
+function rerenderMenu(activeHabbit) {
+    if (!activeHabbit) {
+        return;
+    }
+
+    for (const habbit of habbits) {
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+        if (!existed) {
+            const element = document.createElement('button');
+            element.setAttribute('menu-habbit-id', habbit.id);
+            element.classList.add('menu__item');
+            element.innerHTML = `<img src="images/icons/${habbit.icon}.svg" alt="${habbit.name}">`;
+
+            if (activeHabbit.id === habbit.id) {
+                element.classList.add('menu__item_active');
+            }
+
+            element.addEventListener('click', () => rerender(habbit.id));
+
+            page.menu.appendChild(element);
+            continue;
+        }
+        
+        if (activeHabbit.id === habbit.id) {
+            existed.classList.add('menu__item_active');
+        } else {
+            existed.classList.remove('menu__item_active');
+        }
+    }
+}
+
+function rerender(activeHabbitId) {
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+    rerenderMenu(activeHabbit);
+}
+
+
+/* init */
 
 (() => {
     loadData();
+    rerender(habbits[0].id);
 })();
